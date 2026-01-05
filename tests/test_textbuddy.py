d8fe6aa8d1a80efa3913a82fedb249b7364cb2d1
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-05 07:50:49 -0500

    Add tests for top_words

--- tests/test_textbuddy.py
+++ tests/test_textbuddy.py
@@ -13,3 +13,10 @@ def test_stats_counts():
     assert s.lines == 2
     assert s.words == 2
     assert s.unique_words == 2
+
+
+def test_top_words():
+    text = "a a a b b c"
+    tops = top_words(text, 2)
+    assert tops[0] == ("a", 3)
+    assert tops[1] == ("b", 2)
